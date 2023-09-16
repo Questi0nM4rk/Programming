02510c047ec5ec70f68800776f8f33042e7e1223
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-09-16 18:11:18 +0200

    lol

--- Python/RequestSender/RequestSender.py
+++ Python/RequestSender/RequestSender.py
@@ -2,43 +2,130 @@ import argparse
 import requests
 import time
 from datetime import datetime
+from selenium import webdriver
 
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description="Send requests for a specified duration starting at a given time")
-parser.add_argument("-u", "--url", required=True, help="URL to send requests to")
-parser.add_argument("-c", "--count", required=True, type=int, help="Number of requests per second")
-parser.add_argument("-t", "--time", required=True, help="Start time in the format '31/8/2023-20:23:36'")
-parser.add_argument("-s", "--seconds", required=True, type=int, help="Duration in seconds")
-args = parser.parse_args()
+class cookie:
+    name: str
+    value: str
 
-# Convert the provided start time to a datetime object
-start_time = datetime.strptime(args.time, "%d/%m/%Y-%H:%M:%S")
-
-# Calculate the time to wait before sending requests
-current_time = datetime.now()
-time_to_wait = (start_time - current_time).total_seconds()
-
-if time_to_wait > 0:
-    print(f"Waiting {time_to_wait:.2f} seconds until the specified start time.")
-    time.sleep(time_to_wait)
-
-# Calculate end time
-end_time = time.time() + args.seconds
-
-# Send requests for the specified duration
-while time.time() < end_time:
-    for _ in range(args.count):
-        response = requests.get(args.url)
-        print(f"Response status code: {response.status_code}")
+    def __init__(self, name: str, value: str) -> None:
+        self.name = name
+        self.value = value
+        
     
-    # Sleep for 1 second
-    time.sleep(1)
+class bot:
+    cookies: list[cookie]
+    url: str
+    time: str
+    end_time: int
+    count: int
+    seconds: int
 
-    # Print a separator line between each second's requests
-    print("=" * 30)
+    
+    def __init__(self, url, time, count, seconds) -> None:
+        self.url = url
+        self.time = time
+        self.count = count
+        self.seconds = seconds
+        self.end_time
+    
+    
+    def cookies_to_dict(self):
+        cookies_dict = {}
+        for cookie in self.cookies:
+            cookies_dict[cookie.name] = cookie.value
+        return cookies_dict    
+    
+    
+    def get_cookies(self):
+        # Create a new Chrome browser instance
+        driver = webdriver.Chrome()  # Replace with the path to your chromedriver executable
 
-print("Request sending complete.")
+        # Navigate to the website and log in manually
+        driver.get('https://muni.islogin.cz/login/-dkNKkBh2fmbeAwUJOEKNfhG?lang=en')
+
+        input()
+
+        # After manually logging in, you can retrieve the cookies
+        cookies = driver.get_cookies()
+        for cookie in cookies:
+            print(f"Cookie Name: {cookie['name']}")
+            print(f"Cookie Value: {cookie['value']}")
+
+        # Close the browser
+        driver.quit()
+        
+    
+    def send_requests(self):
+        # Convert the provided start time to a datetime object
+        start_time = datetime.strptime(self.time, "%d/%m/%Y-%H:%M:%S")
+
+        # Calculate the time to wait before sending requests
+        current_time = datetime.now()
+        time_to_wait = (start_time - current_time).total_seconds()
+
+        if time_to_wait > 0:
+            print(f"Waiting {time_to_wait:.2f} seconds until the specified start time.")
+            time.sleep(time_to_wait)
+
+        # Calculate end time
+        end_time = time.time() + self.seconds
+
+        # Send requests for the specified duration
+        while time.time() < end_time:
+            for _ in range(self.count):
+                #response = requests.get(self.url, cookies=self.cookies_to_dict())
+                #print(f"Response status code: {response.status_code}")
+                print("Request sent!")
+            
+            # Sleep for 1 second
+            time.sleep(1)
 
 
+    def parser(self):
+        # Parse command-line arguments
+        parser = argparse.ArgumentParser(description="Send requests for a specified duration starting at a given time")
+        parser.add_argument("-u", "--url", required=True, help="URL to send requests to")
+        parser.add_argument("-c", "--count", required=True, type=int, help="Number of requests per second")
+        parser.add_argument("-t", "--time", required=True, help="Start time in the format '31/8/2023-20:23:36'")
+        parser.add_argument("-s", "--seconds", required=True, type=int, help="Duration in seconds")
+        args = parser.parse_args()
+
+        # Convert the provided start time to a datetime object
+        start_time = datetime.strptime(args.time, "%d/%m/%Y-%H:%M:%S")
+
+        # Calculate the time to wait before sending requests
+        current_time = datetime.now()
+        time_to_wait = (start_time - current_time).total_seconds()
+
+        if time_to_wait > 0:
+            print(f"Waiting {time_to_wait:.2f} seconds until the specified start time.")
+            time.sleep(time_to_wait)
+
+        # Calculate end time
+        self.end_time = time.time() + args.seconds
+
+    def send(self):
+        # Send requests for the specified duration
+        while time.time() < self.end_time:
+            for _ in range(self.count):
+                response = requests.get(self.url)
+                print(f"Response status code: {response.status_code}")
+            
+            # Sleep for 1 second
+            time.sleep(1)
+
+            # Print a separator line between each second's requests
+            print("=" * 30)
+
+        print("Request sending complete.")
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
 
--- Python/RequestSender/tests.py
+++ Python/RequestSender/tests.py
@@ -1,33 +1,84 @@
-import shutil
-import os
-import json
-import sqlite3
+from selenium import webdriver
+import requests
+from datetime import datetime
 
-PATH = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google",
-                    "Chrome", "User Data", "Default", "Network", "Cookies")
+class cookie:
+    name: str
+    value: str
 
-DESTPATH = os.path.join(os.getcwd(), "Cookies_copy.db")
-
-
-
-def main():
-    if not os.path.exists(PATH):
-        print("Database was not found...")
-        return
-
-    shutil.copy(PATH, DESTPATH)
-
-    connection = sqlite3.connect(DESTPATH)
-    cursor = connection.cursor()
+    def __init__(self, name: str, value: str) -> None:
+        self.name = name
+        self.value = value
+        
     
-    cursor.execute("SELECT * FROM cookies")
-    
-    cookies = [dict(row) for row in cursor.fetchall()]
-    
-    with open('cookies.json', 'w') as json_file:
-        json.dump(cookies, json_file, indent=4)
 
-    connection.close()
+class bot:
+    cookies: list[cookie]
+    url: str
+    time: str
+    count: int
+    seconds: int
 
-if __name__ == "__main__":
-    main()
+    
+    def __init__(self, url, time, count, seconds) -> None:
+        self.url = url
+        self.time = time
+        self.count = count
+        self.seconds = seconds
+    
+    
+    def cookies_to_dict(self):
+        cookies_dict = {}
+        for cookie in self.cookies:
+            cookies_dict[cookie.name] = cookie.value
+        return cookies_dict    
+    
+    
+    def get_cookies(self):
+        # Create a new Chrome browser instance
+        driver = webdriver.Chrome()  # Replace with the path to your chromedriver executable
+
+        # Navigate to the website and log in manually
+        driver.get('https://muni.islogin.cz/login/-dkNKkBh2fmbeAwUJOEKNfhG?lang=en')  # Replace with the URL of the website you want to log in to
+
+        input()
+
+        # After manually logging in, you can retrieve the cookies
+        cookies = driver.get_cookies()
+        for cookie in cookies:
+            print(f"Cookie Name: {cookie['name']}")
+            print(f"Cookie Value: {cookie['value']}")
+
+        # Close the browser
+        driver.quit()
+        
+    
+    def send_request(self, time, ):
+        # Convert the provided start time to a datetime object
+        start_time = datetime.strptime(self.time, "%d/%m/%Y-%H:%M:%S")
+
+        # Calculate the time to wait before sending requests
+        current_time = datetime.now()
+        time_to_wait = (start_time - current_time).total_seconds()
+
+        if time_to_wait > 0:
+            print(f"Waiting {time_to_wait:.2f} seconds until the specified start time.")
+            time.sleep(time_to_wait)
+
+        # Calculate end time
+        end_time = time.time() + self.seconds
+
+        # Send requests for the specified duration
+        while time.time() < end_time:
+            for _ in range(self.count):
+                #response = requests.get(self.url, cookies=self.cookies_to_dict())
+                #print(f"Response status code: {response.status_code}")
+                print("Request sent!")
+            
+            # Sleep for 1 second
+            time.sleep(1)
+
+
+
+class IS:
+    Bot: bot        
