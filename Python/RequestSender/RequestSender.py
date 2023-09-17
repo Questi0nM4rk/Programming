8e629a2d900e7a5964036cdedb1fefa5079fa6fa
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-09-17 02:56:41 +0200

    just cannot sleep

--- Python/RequestSender/RequestSender.py
+++ Python/RequestSender/RequestSender.py
@@ -9,7 +9,6 @@ from enum import Enum
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 from dotenv import load_dotenv
 from os import getenv
