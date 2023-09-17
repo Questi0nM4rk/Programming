196a93f75fb87d711666222dcb23e6b205582f9c
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-09-17 02:38:54 +0200

    lol

--- Python/RequestSender/RequestSender.py
+++ Python/RequestSender/RequestSender.py
@@ -2,6 +2,7 @@ import argparse
 import requests
 from datetime import datetime, timedelta
 import time
+import json
 
 from enum import Enum
 
@@ -22,9 +23,65 @@ LOGIN_URL = getenv("LOGIN_URL")
 class RUN(Enum):                # user primarly for testing
     VISIBLE = True
     NOT_VISIBLE = False
+
+class Cookie():
+    domain: str
+    expiry: int
+    httpOnly: bool
+    name: str
+    path: str
+    sameSite: str
+    secure: bool
+    value: str
     
+    def __init__(self, domain, expiry, httpOnly, name, path,
+                 sameSite, secure, value) -> None:
+        
+        self.domain = domain
+        self.expiry = expiry
+        self.httpOnly = httpOnly
+        self.name = name
+        self.path = path
+        self.sameSite = sameSite
+        self.secure = secure
+        self.value = value
+        
+        
+    def is_expired(self) -> bool:
+        the_date = datetime.utcfromtimestamp(self.expiry)
+        
+        return datetime.now() > the_date
+    
+    def to_dict(self) -> dict:
+        cookie_dict = {
+            "domain": self.domain,
+            "expiry": self.expiry,
+            "httpOnly": self.httpOnly,
+            "name": self.name,
+            "path": self.path,
+            "sameSite": self.sameSite,
+            "secure": self.secure,
+            "value": self.value
+        }
+        
+        return cookie_dict
+    
+    @classmethod
+    def from_dict(cls, cookie_dict: dict) -> "Cookie":
+        return cls(
+            cookie_dict["domain"],
+            cookie_dict["expiry"],
+            cookie_dict["httpOnly"],
+            cookie_dict["name"],
+            cookie_dict["path"],
+            cookie_dict["sameSite"],
+            cookie_dict["secure"],
+            cookie_dict["value"]
+        )
+        
+
 class Bot:
-    cookies: list[dict[str, str]]
+    cookies: list[Cookie]
     
     start_time: datetime
     end_time: datetime
@@ -34,11 +91,6 @@ class Bot:
     def __init__(self) -> None:
         self.cookies = []
         
-    
-    def cookies_dict(self) -> dict[str, str]:
-        
-        return self.cookies[0]
-    
     
     def send_requests(self, url, when, count, seconds) -> None:
         
@@ -62,10 +114,10 @@ class Bot:
         while datetime.now() <= self.end_time:
             if self.cookies:
                 for _ in range(count):
-                    response = requests.get(url, cookies=self.cookies_dict())
+                    #response = requests.get(url, cookies=self.cookies)
                     print(url)
             
-            time.sleep(0.99)
+            time.sleep(1)
 
 
 class Chrome:
@@ -90,6 +142,22 @@ class Chrome:
         
         
     def login(self, url, username, password) -> None:
+        
+        cookie_data = {}
+        with open("secret.json", "r") as f:
+            cookie_data = json.load(f)
+            
+        print(cookie_data)
+            
+        if cookie_data:
+            
+            cookie = Cookie.from_dict(cookie_data[0])
+            
+            if not cookie.is_expired():
+                self.client.cookies.append(cookie)
+                return
+         
+        
         self.driver.get(url)
         
         username_field = self.driver.find_element(By.NAME, "credential_0")        
@@ -101,9 +169,12 @@ class Chrome:
         
         submit_button.click()
         
-        self.client.cookies = self.driver.get_cookies()
+        cookie_dict = self.driver.get_cookies()
         
-        print(self.client.cookies)
+        self.client.cookies.append(Cookie.from_dict(cookie_dict[0]))
+        
+        with open("secret.json", "w") as f:
+            json.dump(cookie_dict, f, indent=4)
         
         self.driver.quit()
         
@@ -125,7 +196,7 @@ def main():
     parser.add_argument("-s", "--seconds", required=True, type=int, help="Duration in seconds")
     args = parser.parse_args()
     
-    google = Chrome(RUN.VISIBLE)
+    google = Chrome(RUN.NOT_VISIBLE)
     
     google.login(LOGIN_URL, USERNAME, PASSWORD)
     google.send_request(args.url, args.count, args.time, args.seconds)
--- Python/RequestSender/tests.py
+++ Python/RequestSender/tests.py
@@ -1,14 +1,6 @@
-import argparse
+import datetime
 
+timestamp = 1694995217
+expiry_datetime = datetime.datetime.utcfromtimestamp(timestamp)
 
-
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description="Send requests for a specified duration starting at a given time")
-parser.add_argument("-u", "--url", required=True, help="URL to send requests to")
-parser.add_argument("-c", "--count", required=True, type=int, help="Number of requests per second")
-parser.add_argument("-t", "--time", required=True, help="Start time in the format '31/8/2023-20:23:36'")
-parser.add_argument("-s", "--seconds", required=True, type=int, help="Duration in seconds")
-args = parser.parse_args()
-
-print("args")
-print(args.url)
+print("Cookie Expiry Date and Time:", expiry_datetime)
