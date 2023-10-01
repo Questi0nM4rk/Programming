4fe73395a0e7ccce2c46d9c9eb08985f9ba28b8a
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-10-01 21:29:33 +0200

    lol

--- Python/BoostedKnightPath/solution.py
+++ Python/BoostedKnightPath/solution.py
@@ -17,21 +17,18 @@ MOVES = [
 ]
 
 
-
 class BoostedKnight():
-    Path: list[str]
-    Board: list[int]
+    path: list[str]
+    board: list[int]
 
     def __init__(self, from_x, from_y, counter, board) -> None:
-        self.Path = []
-        self.Board = board
-        self.Board[from_x][from_y] = counter
+        self.path = []
+        self.board = board
+        self.board[from_x][from_y] = counter
 
         self.spawn_knights(from_x, from_y, counter)
 
-
     def spawn_knights(self, x, y, counter) -> bool:
-        
         for move in MOVES:
             if counter >= 6:
                 return True
@@ -40,10 +37,11 @@ class BoostedKnight():
                 new_x = x + move[0]
                 new_y1 = y + move[1][0]
                 new_y2 = y + move[1][1]
-                
+
                 if self.new_knight(new_x, new_y1, counter + 1) and self.new_knight(new_x, new_y2, counter + 1):
                     self.write_move(new_x, y1=new_y1, y2=new_y2)
                     return True
+
             else:
                 new_x1 = x + move[0][0]
                 new_x2 = x + move[0][1]
@@ -55,36 +53,33 @@ class BoostedKnight():
 
         if x < SIZE:
             self.spawn_knights(x + 1, y, counter)
-            
+
         elif x >= SIZE and y < SIZE:
             self.spawn_knights(0, y + 1, counter)
 
-        self.Path.pop()
+        self.path.pop()
         return False
 
-
     def new_knight(self, x, y, counter) -> bool:
-        if not (0 <= x < SIZE and 0 <= y < SIZE and self.Board[x][y] == 0):
+        if not (0 <= x < SIZE and 0 <= y < SIZE and self.board[x][y] == 0):
             return False
         return self.spawn_knights(x, y, counter)
 
-
-    def write_move(self, x1, y1, x2 = None, y2 = None):
+    def write_move(self, x1, y1, x2=None, y2=None):
         notation = "qK"
         if x2:
             notation += chr(ord("a") + x1) + str(y1) + "&" + chr(ord("a") + x2) + str(y1)
         elif y2:
             notation += chr(ord("a") + x1) + str(y1) + "&" + chr(ord("a") + x1) + str(y2)
-        
-        self.Path.append(notation)
 
+        self.path.append(notation)
 
 
 def main():
     board = np.zeros((SIZE, SIZE))
     b_knight = BoostedKnight(0, 0, 1, board)
 
-    if b_knight.Path:
+    if b_knight.path:
         print("Path found!!!")
 
 
