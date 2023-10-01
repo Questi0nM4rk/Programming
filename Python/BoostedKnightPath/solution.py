2b3a3fc6d9d0ab50a0173dfa5198d6fb5010e0aa
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-10-01 22:33:38 +0200

    s

--- Python/BoostedKnightPath/solution.py
+++ Python/BoostedKnightPath/solution.py
@@ -8,6 +8,11 @@ exml:
     qKc4&e4
 """
 
+#TODO:
+# check board if full
+#
+#
+
 SIZE = 8
 MOVES = [
     [2, (1, -1)],
@@ -26,7 +31,9 @@ class BoostedKnight():
         self.board = board
         self.board[from_x][from_y] = counter
 
-        self.spawn_knights(from_x, from_y, counter)
+        for y in range(SIZE):
+            for x in range(SIZE):
+                self.spawn_knights(x, y, counter)
 
     def spawn_knights(self, x, y, counter) -> bool:
         for move in MOVES:
@@ -39,7 +46,7 @@ class BoostedKnight():
                 new_y2 = y + move[1][1]
 
                 if self.new_knight(new_x, new_y1, counter + 1) and self.new_knight(new_x, new_y2, counter + 1):
-                    self.write_move(new_x, y1=new_y1, y2=new_y2)
+                    self.write_move(counter, new_x, y1=new_y1, y2=new_y2)
                     return True
 
             else:
@@ -48,14 +55,8 @@ class BoostedKnight():
                 new_y = y + move[1]
 
                 if self.new_knight(new_x1, new_y, counter + 1) and self.new_knight(new_x2, new_y, counter + 1):
-                    self.write_move(x1=new_x1, x2=new_x2, y1=new_y)
+                    self.write_move(counter, x1=new_x1, x2=new_x2, y1=new_y)
                     return True
-
-        if x < SIZE:
-            self.spawn_knights(x + 1, y, counter)
-
-        elif x >= SIZE and y < SIZE:
-            self.spawn_knights(0, y + 1, counter)
 
         self.path.pop()
         return False
@@ -65,12 +66,16 @@ class BoostedKnight():
             return False
         return self.spawn_knights(x, y, counter)
 
-    def write_move(self, x1, y1, x2=None, y2=None):
+    def write_move(self, counter, x1, y1, x2=None, y2=None):
         notation = "qK"
         if x2:
             notation += chr(ord("a") + x1) + str(y1) + "&" + chr(ord("a") + x2) + str(y1)
+            self.board[x1][y1] = counter
+            self.board[x2][y1] = counter
         elif y2:
             notation += chr(ord("a") + x1) + str(y1) + "&" + chr(ord("a") + x1) + str(y2)
+            self.board[x1][y1] = counter
+            self.board[x1][y2] = counter
 
         self.path.append(notation)
 
