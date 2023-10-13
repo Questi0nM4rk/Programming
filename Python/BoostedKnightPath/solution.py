16cdc43e81378725119b215e580fcce95b059e52
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-10-13 04:08:34 +0200

    lol

--- Python/BoostedKnightPath/solution.py
+++ Python/BoostedKnightPath/solution.py
@@ -16,32 +16,29 @@ exml:
 # if false erase the knights that were placed on board
 #
 
-SIZE = 7
+SIZE = 8
 MOVES_X = [2, 1, -1, -2, -2, -1,  1,  2]
 MOVES_Y = [1, 2,  2,  1, -1, -2, -2, -1]
 
-class NomalKnight():
+class NormalKnight():
     
-    def __init__(self, x, y, board, adds = 0) -> None:
-        self.board = board
+    def __init__(self, board) -> None:
         self.last_move = ()
-        counter = 2
-        self.board[x,y] = 1
-        print(self.solve(x, y, board, counter, adds))
+        self.board = board
         
-    def solve(self, x, y, board, counter, adds):
-        if counter >= adds:
-            self.last_move = (x,y)
-            return True
+    def solve(self, x, y, split):
+
+        if not self.board[x][y]:
+            self.board[x][y] = True
 
         for i in range(8):
             n_x = x + MOVES_X[i]
             n_y = y + MOVES_Y[i]
             if self.validate_move(n_x, n_y):
-                self.board[n_x, n_y] = counter
-                if self.solve(n_x, n_y, board, counter + 1, adds):
+                self.board[n_x, n_y] = True
+                if BoostedKnight(self.board).solve(n_x, n_y, split-1):
                     return True
-                self.board[n_x, n_y] = 0
+                self.board[n_x, n_y] = False
         return False
     
     def validate_move(self, x, y):
@@ -50,42 +47,23 @@ class NomalKnight():
         return False
 
 
-"""
 class BoostedKnight():
-    path: list[str]
-    board: list[list[bool]]
-
-    def __init__(self, from_x, from_y, board) -> None:
-        self.path = []
+    def __init__(self, board) -> None:
         self.board = board
-        self.board[from_x][from_y] = True
-
-        split = SIZE
-        print(split)
-        input()
-        print(self.spawn_knights(from_x, from_y, split))
-
-    def spawn_knights(self, x, y, split) -> bool:
-        if self.check_board():
+        self.path = []
+        
+    def solve(self, x, y, split) -> bool:
+        if split == 0:
             return True
 
-        if split <= 0:
-            return False
-
+        k1 = NormalKnight(self.board).solve(x,y,split)
+        k2 = NormalKnight(self.board).solve(x,y,split)
+        
+        if k1 and k2:
+            #self.write_move()
+            return True
         return False
     
-    def validate_move(self, x: int, y: int):
-        if x < 8 and x >= 0 and y < 8 and y >= 0 and self.board[x][y] == 0:
-            return True
-        return False
-
-    def new_knight(self, x, y, split) -> bool:
-        if not (0 <= x < SIZE and 0 <= y < SIZE): #and self.board[x][y] == 0):
-            return False
-        split -= 1
-        print(split)
-        return self.spawn_knights(x, y, split)
-
     def write_move(self, x1, y1, x2=None, y2=None):
         notation = "qK"
         if x2:
@@ -99,39 +77,7 @@ class BoostedKnight():
 
         self.path.append(notation)
         
-    def delete_moves(self, x1, y1, x2=None, y2=None):
-        notation = "qK"
-        if x2:
-            notation += chr(ord("a") + x1) + str(y1+1) + "&" + chr(ord("a") + x2) + str(y1+1)
-        elif y2:
-            notation += chr(ord("a") + x1) + str(y1+1) + "&" + chr(ord("a") + x1) + str(y2+1)
-            
-        _from = self.path.index(notation)
-        rem = self.path[:_from]
-        self.path = self.path[:_from]
         
-        for move in rem:
-            move = move[2:]
-            _first, _second = move.split("&")
-
-            _first_x = int(ord(_first[0]) - ord("a"))
-            _first_y = int(_first[1]) - 1
-
-            _second_x = ord(_second[0]) - ord("a")
-            _second_y = int(_second[1]) - 1
-            
-            self.board[_first_x][_first_y] = False
-            self.board[_second_x][_second_y] = False
-            
-        
-    def check_board(self) -> bool:
-        return bool(np.all(self.board == 1))
-
-"""
-class BoostedKnight():
-    def __init__(self, x, y, board) -> None:
-        self.board = board
-
 
 def validateMove(bo, row, col):
     if row < SIZE and row >= 0 and col < SIZE and col >= 0 and bo[row, col] == 0:
@@ -154,22 +100,11 @@ def solve (bo, row, col, counter):
 def main():
     
     board = np.zeros((SIZE, SIZE))
-    #solve(board, 0, 0, 1)
     
-    n_knight = NomalKnight(0,0,board)
+    boostedK = BoostedKnight(board)
+    boostedK.solve(0,0,19)
     
-    print(n_knight.board)
-    """x,y = n_knight.last_move
-    n2_knight = NomalKnight(x,y,n_knight.board, 1)
-    
-    print(n2_knight.board)"""
-    
-    """
-    b_knight = BoostedKnight(0, 0, board)
-
-    if b_knight.path:
-        print("Path found!!!")
-    print(b_knight.board)"""
+    print(boostedK.board)
 
 if __name__ == "__main__":
     main()
