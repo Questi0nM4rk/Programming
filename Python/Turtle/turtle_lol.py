021266551631d36dbd1af813c3c8acf567a042dc
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-10-27 12:50:30 +0200

    lol

--- Python/Turtle/turtle_lol.py
+++ Python/Turtle/turtle_lol.py
@@ -1,5 +1,5 @@
-import turtle as t
-from datetime import datetime
+from turtle import penup, pendown, forward, setheading, right, left, done
+from math import sin, pi
 
 # Napište proceduru ‹clock›, která pomocí želví grafiky vykreslí ciferník
 # hodin s ručičkami v následující podobě:
@@ -28,99 +28,101 @@ from datetime import datetime
 # Použití procedur ‹speed›, ‹delay› a ‹done› se sice nepovažuje za chybu,
 # ale budou v testech ignorovány, tj. «nebudou mít žádný efekt».
 
-
-"""
-move it to the ib111
-how to make the clock arms so they are moved from the center
-
-"""
-
 def _draw_clock(side):
     angle = 360//12
-    t.penup()
-    t.left(90)
-    t.forward(side*2)
-    t.right(105)
-    t.pendown()
+    penup()
+    left(90)
+    forward(side*2)
+    right(105)
+    pendown()
     for _ in range(12):
-        t.forward(side)
-        t.right(angle)
+        forward(side)
+        right(angle)
 
 
 def _draw_clock_arm(angle, side, version):
-    t.penup()
-    t.goto(0, 0)
+    penup()
     if version is 0:    # seconds
         sec_len = 1.8*side
-        t.setheading(90 - angle)
-        t.pendown()
-        t.forward(sec_len)
+        setheading(90 - angle)
+        pendown()
+        forward(sec_len)
     elif version is 1:  # minites
         min_len = side*1.6
         min_width = side/20
-        t.setheading(90-angle)
-        t.pendown()
-        t.right(90)
-        t.forward(min_width/2.0)
-        t.left(90)
-        t.forward(min_len)
-        t.left(90)
-        t.forward(min_width)
-        t.left(90)
-        t.forward(min_len)
-        t.left(90)
-        t.forward(min_width/2.0)
+        setheading(90-angle)
+        right(180)
+        forward(min_width/2.0)
+        pendown()
+        left(90)
+        forward(min_width/2.0)
+        left(90)
+        forward(min_len)
+        left(90)
+        forward(min_width)
+        left(90)
+        forward(min_len)
+        left(90)
+        forward(min_width/2.0)
+        penup()
+        left(90)
+        forward(min_width/2.0)
+        pendown()
     elif version is 2:  # hours
         hs_len = side*1.4
         hs_width = side/10
-        t.setheading(90-angle)
-        t.pendown()
-        t.right(90)
-        t.forward(hs_width/2.0)
-        t.left(90)
-        t.forward(hs_len)
-        t.left(90)
-        t.forward(hs_width)
-        t.left(90)
-        t.forward(hs_len)
-        t.left(90)
-        t.forward(hs_width/2.0)
+        setheading(90-angle)
+        right(180)
+        forward(hs_width/2.0)
+        pendown()
+        left(90)
+        forward(hs_width/2.0)
+        left(90)
+        forward(hs_len)
+        left(90)
+        forward(hs_width)
+        left(90)
+        forward(hs_len)
+        left(90)
+        forward(hs_width/2.0)
+        penup()
+        left(90)
+        forward(hs_width/2.0)
+        pendown()
+        
 
 
-def _calculate_clock_angles():
-    now = datetime.now()
-    hour = now.hour
-    minute = now.minute
-    second = now.second
-
-    hour_angle = (hour % 12) * 30 + (minute / 2) + (second / 120)
-    minute_angle = (minute % 60) * 6 + (second / 10)
-    second_angle = (second % 60) * 6
+def _calculate_clock_angles(epoch_time):
+    hour_angle = (epoch_time % 43200) * (360 / 43200.0)
+    minute_angle = (epoch_time % 3600) * (360 / 3600.0)
+    second_angle = (epoch_time % 60) * (360 / 60.0)
 
     return hour_angle, minute_angle, second_angle
 
 
 def clock(epoch_time, side):
     _draw_clock(side)
+    rad = side / (2 * (sin(pi / 12)))
+    penup()
+    right(75)
+    forward(rad)
+    left(90)
+    pendown()
 
-    t.penup()
-    t.right(90)
-    t.forward(2*side)
-    t.left(90)
-    t.pendown()
+    hour_angle, minute_angle, second_angle = _calculate_clock_angles(epoch_time)
 
-    hour_angle, minute_angle, second_angle = _calculate_clock_angles()
-
+    print(hour_angle, minute_angle, second_angle)
+    
     _draw_clock_arm(hour_angle, side, 2)
     _draw_clock_arm(minute_angle, side, 1)
     _draw_clock_arm(second_angle, side, 0)
 
-    t.done()
+    done()
 
 
 def main():
     clock(1661081862, 150.0)
-    t.done()
+    done()
 
 
 if __name__ == '__main__':
