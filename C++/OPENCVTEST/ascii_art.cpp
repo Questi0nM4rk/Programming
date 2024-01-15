553f64608545747bca85908272fad2c7e6ba5731
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2024-01-15 21:47:49 +0100

    lol

---
+++ C++/OPENCVTEST/ascii_art.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+/*
+
+greyscale = (r + g + b) / 3;
+
+luminance = r * 0.3 + g * 0.59 + b * 0.11;
+
+saturation = (max(r, g, b) + min(r, g, b)) / 2;
+
+*/
+
+
+int main() {
+
+    std::string ascii_map = "@#&8WMBRXVYILT!1234567;:^~_,.`   ";
+
+
+
+}
